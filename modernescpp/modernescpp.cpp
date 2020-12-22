ba55653ec8f8ddf1ee998f1b936d7f432012c47d
唐韬 <inserlan@163.com>
2020-12-22 09:52:28 +0800

    add thread safe init

--- modernescpp/modernescpp.cpp
+++ modernescpp/modernescpp.cpp
@@ -2,38 +2,52 @@
 //
 
 #include <iostream>
-#include <thread>
-#include <chrono>
-using namespace std::chrono_literals;
+#include <stdexcept>
 
-class sleeper
+constexpr int factorial(int n)
 {
-public:
-    sleeper(int& i_) : i{ i_ } {}
-    void operator() (int k)
-    {
-        for (size_t j = 0; j < 5; j++)
-        {
-            std::this_thread::sleep_for(100ms);
-            i += k;
-        }
-        std::cout << std::this_thread::get_id() << std::endl;
-    }
+    return n <= 1 ? 1 : (n * factorial(n - 1));
+}
 
-private:
-    int& i;
+class conststr
+{
+    const char* p;
+    std::size_t sz;
+
+public:
+    template<std::size_t N>
+    constexpr conststr(const char(&a)[N]) : p(a), sz(N) {}
+
+    constexpr char operator[](std::size_t n) const
+    {
+        return n < sz ? p[n] : throw std::out_of_range("");
+    }
+    constexpr std::size_t size() const { return sz; }
 };
 
-int main()
+constexpr std::size_t countlower(conststr s, std::size_t n = 0, std::size_t c = 0)
 {
-    std::cout << std::endl;
+    return n == s.size() ? c : 
+        'a' <= s[n] && s[n] <= 'z' ? countlower(s, n + 1, c + 1) :
+        countlower(s, n + 1, c);
+}
 
-    int valSleeper = 1000;
-    std::thread t(sleeper(valSleeper), 5);
-    t.join();
-    std::cout << "valSleeper = " << valSleeper << std::endl;
+template<int n>
+struct constN
+{
+    constN() { std::cout << n << '\n'; }
+};
 
-    std::cout << std::endl;
+int main() {
+
+    std::cout << "4! = ";
+    constN<factorial(4)> out1; // computed at compile time
+
+    volatile int k = 8; // disallow optimization using volatile
+    std::cout << k << "! = " << factorial(k) << '\n'; // computed at run time
+
+    std::cout << "the number of lowercase letters in \"Hello, world!\" is ";
+    constN<countlower("Hello, world!")> out2; // implicitly converted to conststr
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
