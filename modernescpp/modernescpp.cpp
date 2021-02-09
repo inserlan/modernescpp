1e228f5d6fbd1a636c7e831fc37ccc645a3b0f4b
唐韬 <inserlan@163.com>
2021-02-09 10:01:13 +0800

    add atomic flag

--- modernescpp/modernescpp.cpp
+++ modernescpp/modernescpp.cpp
@@ -1,85 +1,38 @@
-#include <iostream>
-#include <utility>
+#include <atomic>
 #include <thread>
-#include <chrono>
-#include <future>
 
-void f1(int n)
+class Spinlock
 {
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "Thread 1 executing\n";
-        ++n;
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-}
+	std::atomic_flag flag;
 
-void f2(int& n)
-{
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "Thread 2 executing\n";
-        ++n;
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-}
-
-class foo
-{
 public:
-    void bar()
-    {
-        for (int i = 0; i < 5; ++i) {
-            std::cout << "Thread 3 executing\n";
-            ++n;
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
-    }
-    int n = 0;
+	Spinlock() : flag(ATOMIC_FLAG_INIT) {}
+
+	void Lock()
+	{
+		while (flag.test_and_set());
+	}
+
+	void Unlock()
+	{
+		flag.clear();
+	}
 };
 
-class baz
-{
-public:
-    void operator()()
-    {
-        for (int i = 0; i < 5; ++i) {
-            std::cout << "Thread 4 executing\n";
-            ++n;
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
-    }
-    int n = 0;
-};
+Spinlock spin;
 
-void doTheWork()
+void WorkOnResource()
 {
-    std::cout << "Processing shared data." << std::endl;
-}
-
-void waitingForWork(std::future<void>&& fut)
-{
-    std::cout << "Worker: Waiting for work." << std::endl;
-    fut.wait();
-    doTheWork();
-    std::cout << "Work done." << std::endl;
-}
-
-void setDataReady(std::promise<void>&& prom)
-{
-    std::cout << "Sender: Data is ready." << std::endl;
-    prom.set_value();
+	spin.Lock();
+	// shared resource
+	spin.Unlock();
 }
 
 int main()
 {
-    std::cout << std::endl;
+	std::thread t(WorkOnResource);
+	std::thread t2(WorkOnResource);
 
-    std::promise<void> sendReady;
-    auto fut = sendReady.get_future();
-    std::thread t1(waitingForWork, std::move(fut));
-    std::thread t2(setDataReady, std::move(sendReady));
-
-    t1.join();
-    t2.join();
-
-    std::cout << "main thread" << std::endl;
+	t.join();
+	t2.join();
 }
