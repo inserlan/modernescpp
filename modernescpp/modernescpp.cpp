a078a87879daade24df127907980b408dca17469
唐韬 <inserlan@163.com>
2021-02-18 09:55:31 +0800

    add atomic boolean

--- modernescpp/modernescpp.cpp
+++ modernescpp/modernescpp.cpp
@@ -1,38 +1,72 @@
-#include <atomic>
+#include <condition_variable>
+#include <iostream>
 #include <thread>
+#include <vector>
+#include <chrono>
+#include <atomic>
 
-class Spinlock
+
+//std::vector<int> mySharedWork;
+//std::mutex mutex_;
+//std::condition_variable condVar;
+//
+//
+//bool dataReady;
+//
+//void waitingForWork()
+//{
+//	std::cout << "Waiting" << std::endl;
+//	std::unique_lock<std::mutex> lck(mutex_);
+//	condVar.wait(lck, [] { return dataReady; });
+//	mySharedWork[1] = 2;
+//	std::cout << "Work done" << std::endl;
+//}
+//
+//void setDataReady()
+//{
+//	mySharedWork = { 1,0,3 };
+//	{
+//		std::lock_guard<std::mutex> lck(mutex_);
+//		dataReady = true;
+//	}
+//	std::cout << "Data prepared" << std::endl;
+//	condVar.notify_one();
+//}
+
+std::vector<int> mySharedWork;
+std::atomic<bool> dataReady(false);
+
+void waitingForWork()
 {
-	std::atomic_flag flag;
-
-public:
-	Spinlock() : flag(ATOMIC_FLAG_INIT) {}
-
-	void Lock()
+	std::cout << "Waiting " << std::endl;
+	while (!dataReady.load())
 	{
-		while (flag.test_and_set());
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));	
 	}
+	mySharedWork[1] = 2;
+	std::cout << "Work done " << std::endl;
+}
 
-	void Unlock()
-	{
-		flag.clear();
-	}
-};
-
-Spinlock spin;
-
-void WorkOnResource()
+void setDataReady()
 {
-	spin.Lock();
-	// shared resource
-	spin.Unlock();
+	mySharedWork = { 1, 0, 3 };
+	dataReady = true;
+	std::cout << "Data prepared" << std::endl;
 }
 
 int main()
 {
-	std::thread t(WorkOnResource);
-	std::thread t2(WorkOnResource);
+	std::cout << std::endl;
+	std::thread t1(waitingForWork);
+	std::thread t2(setDataReady);
 
-	t.join();
+	t1.join();
 	t2.join();
+
+	for (size_t i = 0; i < mySharedWork.size(); i++)
+	{
+		std::cout << mySharedWork[i] << " ";
+	}
+
+	std::cout << "\n\n";
 }
