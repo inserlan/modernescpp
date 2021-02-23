e8494d40702ca213f185870be2359cfffc8f7b62
唐韬 <inserlan@163.com>
2021-02-23 09:21:26 +0800

    add transitivity of the acquire release

--- modernescpp/modernescpp.cpp
+++ modernescpp/modernescpp.cpp
@@ -1,35 +1,48 @@
 #include <atomic>
+#include <iostream>
 #include <thread>
+#include <vector>
 
-class Spinlock {
-    std::atomic_flag flag;
-public:
-    Spinlock() {}
+std::vector<int> mySharedWork;
+std::atomic<bool> dataProduced(false);
+std::atomic<bool> dataConsumed(false);
 
-    void lock() {
-        while (flag.test_and_set(std::memory_order_acquire));
-    }
-
-    void unlock() {
-        flag.clear(std::memory_order_release);
-    }
-};
-
-Spinlock spin;
-
-void workOnResource() {
-    spin.lock();
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    spin.unlock();
+void dataProducer() 
+{
+    mySharedWork = { 1,0,3 };
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    dataProduced.store(true, std::memory_order_release);
 }
 
+void deliveryBoy() 
+{
+    while (!dataProduced.load(std::memory_order_acquire));
+    dataConsumed.store(true, std::memory_order_release);
+}
 
-int main() {
+void dataConsumer() 
+{
+    while (!dataConsumed.load(std::memory_order_acquire));
+    mySharedWork[1] = 2;
+}
 
-    std::thread t(workOnResource);
-    std::thread t2(workOnResource);
+int main() 
+{
 
-    t.join();
+    std::cout << std::endl;
+
+    std::thread t1(dataConsumer);
+    std::thread t2(deliveryBoy);
+    std::thread t3(dataProducer);
+
+    t1.join();
     t2.join();
+    t3.join();
+
+    for (auto v : mySharedWork) {
+        std::cout << v << " ";
+    }
+
+    std::cout << "\n\n";
 
 }
